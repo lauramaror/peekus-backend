80d0c459e68c4a12ae1ded517ca69e935013be5e
lauramaror <lauramaror@gmail.com>
2022-11-02 19:52:39 +0100

    comment api

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,5 +1,6 @@
 const { response } = require('express');
 const { conexionDB } = require('../helpers/configdb');
+const { v4: uuidv4 } = require('uuid');
 
 const getComments = async(req, res = response) => {
     const id = req.query.id || '';
@@ -32,6 +33,105 @@ const getComments = async(req, res = response) => {
     }
 }
 
+const saveComment = async(req, res = response) => {
+    console.log('saveComment');
+    const body = req.body;
+    const text = body.text;
+    const idUser = body.idUser;
+    const idEvent = body.idEvent;
+    try {
+        if ((text && text !== '') && (idUser && await checkIfUserExists(idUser)) && (idEvent && await checkIfEventExists(idEvent))) {
+            let query = 'INSERT INTO \`comment\` VALUES (\'' + uuidv4() + '\', \'' + text + '\', ';
+            query += 'CURRENT_TIMESTAMP, ';
+            query += '\'' + idUser + '\', \'' + idEvent + '\')';
+
+            conexionDB(query, function(err, rows) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.json({
+                        ok: true,
+                        msg: 'Comment created',
+                    });
+                }
+            });
+        } else {
+            response.send("Invalid parameters");
+            response.end();
+            return;
+        }
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            ok: false,
+            msg: 'Error saving comment',
+        });
+    }
+}
+
+const deleteComment = async(req, res = response) => {
+    const id = req.query.id;
+
+    try {
+        if (!id || (id && !(await checkIfCommentExists(id)))) {
+            return res.status(500).json({
+                ok: false,
+                msg: 'Comment does not exist'
+            });
+        }
+
+        let query = 'DELETE FROM \`comment\` WHERE \`id\` = \'' + id + '\';'
+
+        conexionDB(query, function(err, rows) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.json({
+                    ok: true,
+                    msg: 'Comment deleted',
+                });
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Error deleting comment'
+        });
+
+    }
+}
+
+const checkIfCommentExists = (commentId) => {
+    let query = 'SELECT * FROM comment WHERE id=\'' + commentId + '\'';
+    return new Promise(resolve => {
+        conexionDB(query, function(err, rows) {
+            resolve(rows.length > 0);
+        })
+    });
+}
+
+const checkIfUserExists = (userId) => {
+    let query = 'SELECT * FROM user WHERE id=\'' + userId + '\'';
+    return new Promise(resolve => {
+        conexionDB(query, function(err, rows) {
+            resolve(rows.length > 0);
+        })
+    });
+}
+
+const checkIfEventExists = (eventId) => {
+    let query = 'SELECT * FROM event WHERE id=\'' + eventId + '\'';
+    return new Promise(resolve => {
+        conexionDB(query, function(err, rows) {
+            resolve(rows.length > 0);
+        })
+    });
+}
+
 module.exports = {
-    getComments
+    getComments,
+    saveComment,
+    deleteComment
 };
--- routes/comment.js
+++ routes/comment.js
@@ -3,11 +3,17 @@ const { Router } = require('express');
 const router = Router();
 
 const {
-    getComments
+    getComments,
+    saveComment,
+    deleteComment
 } = require('../controllers/comment');
 
 const { validateJWT } = require('../helpers/validatejwt');
 
 router.get('/', [validateJWT], getComments);
+
+router.post('/', [validateJWT], saveComment);
+
+router.delete('/', [validateJWT], deleteComment);
 
 module.exports = router;
